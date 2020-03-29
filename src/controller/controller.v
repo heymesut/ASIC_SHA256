b42781624c0e9e7062daea2bcce58d7ff6b0dd15
heymesut <1299740655@qq.com>
2020-03-29 19:23:19 +0800

    initial commit

---
+++ src/controller/controller.v
@@ -0,0 +1,99 @@
+///////////////////////////////////////
+//description:control signal generator(FSM)
+
+//paramter:description
+//clk
+//reset:hign enable
+//first_block:when input is the first bolck,set 1 during the first cycle and
+//the last cycle before input
+//last_block: when input is the last block,set 1 during the first cycle
+//output_enable: when output digest is valid,set 1
+//busy:when the module is working,set 1
+//inner_busy:set 1 to enable hash_core and message_schedule module
+///////////////////////////////////////    
+
+
+module controller(
+    input  wire               clk,
+    input  wire               reset,
+    input  wire               first_block,
+    input  wire               last_block,
+    output wire               output_enable,
+    output wire               busy,
+    output wire               inner_busy
+);
+
+
+reg  [1:0]         state;
+reg  [1:0]         next_state;
+
+reg  [6:0]         counter1;
+reg  [2:0]         counter2;
+
+parameter s0=2'b00;//idle
+parameter s1=2'b01;//iteration
+parameter s2=2'b10; //output
+
+assign busy             =    (state==s0)? 0 : 1;
+assign output_enable    =    (state==s2)? 1 : 0;
+assign inner_busy       =    (state==s1)? 1 : 0;
+
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset==1'b1)
+        state<=s0;
+    else
+        state<=next_state;
+end
+
+always @(*)
+begin
+    case (state)
+      s0:
+        if(first_block==1)
+            next_state=s1;
+        else
+            next_state=s0;
+      s1:
+        if(counter1==7'd64)
+            next_state=s2;
+        else
+            next_state=s1;
+      s2:
+        if(counter2==3'd7)
+            next_state=s0;
+        else
+            next_state=s2; 
+        default: 
+            next_state=s0;
+    endcase
+end
+
+always @ (posedge clk or posedge reset)
+begin
+    if(reset==1'b1)
+        counter1<=7'd0;
+    else
+        if(counter1==7'd64)
+            counter1<=7'd0;
+        else
+            if(counter1!=7'd0 || last_block==1'b1)
+                counter1<=counter1+1;
+            else
+                counter1<=7'd0;
+end
+
+always @ (posedge clk or posedge reset)
+begin
+    if(reset==1'b1)
+        counter2<=3'd0;
+    else
+        if(state==s2)
+            counter2<=counter2+1;
+        else
+            counter2<=0; 
+end
+
+
+endmodule
---
+++ src/top.v
@@ -0,0 +1,65 @@
+//////////////////////////////////////////////////
+//description：SHA256 top module
+
+//parameter:description
+//clk
+//reset:high enable
+//data:input message(32 bits input per cycle)
+//write_enable:when input data is valid,set 1
+//last_block: when input is the last block,set 1 during the first cycle
+//first_block:when input is the first bolck,set 1 during the first cycle and
+//the last cycle before input
+//busy:when the module is working,set 1
+//digest:output digest(32 bits output per cycle)
+//output_enable: when output digest is valid,set 1
+//////////////////////////////////////////////////
+
+
+module top(
+    input  wire                clk,
+    input  wire                reset,
+    input  wire [31:0]         data,
+    input  wire                write_enable,
+    input  wire                last_block,
+    input  wire                first_block,
+    output wire                busy,
+    output wire [31:0]         digest,
+    output wire                output_enable           
+);
+
+
+wire [31:0]           Wt;
+wire                  inner_busy;//enable hash_core and message_schedule
+
+message_schedule our_message_schedule(
+    .clk(clk),
+    .reset(reset),
+    .data(data),
+    .write_enable(write_enable),
+    .inner_busy(inner_busy),
+    .Wt(Wt)
+);
+
+//hash iteration and hash updating
+hash_core our_hash_core(
+    .clk(clk),
+    .reset(reset),
+    .Wt(Wt),
+    .inner_busy(inner_busy),
+    .first_block(first_block),
+    .output_enable(output_enable),
+    .digest(digest)
+);
+
+controller our_controller(
+    .clk(clk),
+    .reset(reset),
+    .first_block(first_block),
+    .last_block(last_block),
+    .output_enable(output_enable),
+    .busy(busy),
+    .inner_busy(inner_busy)
+);
+
+
+endmodule
