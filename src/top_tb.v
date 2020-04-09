9f5d50f4a7e0698064b0f173a0a0424c287c15be
LKai-Xudexiaohao <derek@126.com>
2020-04-09 15:56:53 +0800

    new testbench with check

--- src/top_tb.v
+++ src/top_tb.v
@@ -1,5 +1,7 @@
 `timescale 1ns / 1ps
 
+`define SHA_QUANSHI0_RESULT 256'hbd03ac1428f0ea86f4b83a731ffc7967bb82866d8545322f888d2f6e857ffc18
+
 module top_tb;
 
     // Inputs
@@ -15,13 +17,21 @@ module top_tb;
     wire [31:0]  digest;
     wire output_enable;
 
+    // temp 
+    reg [31:0] tmp;
+    reg [255:0] result;
+    reg flag;
+    integer i;
+
 //data:00000000000000000000000000000000000000000000000000000000(0 ASCII:8'h30)
-//加密后：0xbd03ac1428f0ea86f4b83a731ffc7967bb82866d8545322f888d2f6e857ffc18
+//加密后：bd03ac1428f0ea86f4b83a731ffc7967bb82866d8545322f888d2f6e857ffc18
 //需要加密的内容是56个0，长度为448位，刚好模512余448，所以被补全，补全到448+512位，补全第一位为1，其余位为0。
 //因为448位二进制表示为111000000，所以最后再加补全64位，64位开始为0，结尾为111000000
     initial begin
         // Inputs
-        //two blocks
+        // two blocks
+        result <=`SHA_QUANSHI0_RESULT;
+        flag<=0;
         clk<=1'b1;
         reset<=1'b1;
         data<=32'b0;
@@ -32,7 +42,7 @@ module top_tb;
             first_block<=1'b1;
         #8  write_enable<=1'b1;
             data<=32'h3030_3030;
-         //the data input begins
+        //the data input begins
         #8  first_block<=1'b0; 
         #104 data<=32'h8000_0000; // the 15th cycle
         #8 data<=32'b0;
@@ -46,6 +56,46 @@ module top_tb;
     end
 
     always #4 clk=~clk;
+
+    always@(posedge clk)
+    if(output_enable == 1'b0)
+    begin
+        flag <= 1'b0;
+    end
+    else
+    if(output_enable == 1'b1)
+        if(flag == 1'b0)
+        begin
+            flag <= 1'b1;
+        end
+        else 
+        begin
+        tmp = result[8*32-1:7*32];
+        //tmp = result[(8-i)*32-1:(7-i)*32];
+        if(digest==tmp)
+            begin
+                $display("OK! Expected %x. Got %x.",tmp,digest);
+                //flag <= 1'b1;
+            end
+        else
+            begin
+                $display("ERROR! Expected %x. Got %x.",tmp,digest);
+                //flag <= 1'b0;
+            end
+        result = result << 32;
+//        case(flag)
+//            3'b000: flag => 3'b001;
+//            3'b001: flag <= 3'b010;
+//            3'b010: flag <= 3'b011;
+//            3'b011: flag <= 3'b100;
+//            3'b100: flag <= 3'b101;
+//            3'b101: flag <= 3'b110;
+//            3'b110: flag <= 3'b111;
+//            3'b111: flag <= 3'b000;
+//        endcase
+        end
+        
+            
 
     top our_top(
         .clk(clk),
